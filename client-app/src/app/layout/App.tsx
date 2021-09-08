611249484c82f19711125ed4583ce44a029cc7a2
chepew23 <jwurrego.co@outlook.com>
2021-09-07 22:13:17 -0500

    feat(layout): :lipstick: Add NavBar component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,27 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, Header, List } from 'semantic-ui-react';
+import NavBar from './NavBar';
 
 function App() {
-  const [activities, setActivities] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:5000/api/activities')
-      .then(response => setActivities(response.data));
-  }, []);
-
   return (
-    <div>
-      <Header as='h2' icon='users' content='Reactivities' />
-
-      <List>
-        {activities.map((activity: any) => (
-          <List.Item key={ activity.id }>
-            { activity.title }
-          </List.Item>
-        ))}
-      </List>
-    </div>
+    <Fragment>
+      <NavBar/>
+      <Container style={{marginTop: '7em'}}>
+      </Container>
+    </Fragment>
   );
 }
 
